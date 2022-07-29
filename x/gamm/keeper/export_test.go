be22fa6daea5fd93ac7edfc1df63a1cf50d8c906
Niccolo Raspa <nraspa@live.it>
2022-07-29 05:56:38 +0200

    Add function
    (cherry picked from commit ccc5afe6c4c513ea1bff97840ae4c37d1c9fd879)
    
    # Conflicts:
    #	x/gamm/keeper/export_test.go

--- x/gamm/keeper/export_test.go
+++ x/gamm/keeper/export_test.go
@@ -3,10 +3,19 @@ package keeper
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
+<<<<<<< HEAD
 	"github.com/osmosis-labs/osmosis/v10/x/gamm/types"
+=======
+	"github.com/osmosis-labs/osmosis/v11/x/gamm/types"
+>>>>>>> ccc5afe (Add function)
 )
 
 // SetParams sets the total set of params.
 func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
+	k.setParams(ctx, params)
+}
+
+// SetParams sets the total set of params.
+func (k Keeper) SetParams2(ctx sdk.Context, params types.Params) {
 	k.setParams(ctx, params)
 }
