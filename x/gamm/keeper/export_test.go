be0814de29cfa7be7af7906323283e3babd35a32
Niccolo Raspa <nraspa@live.it>
2022-07-29 06:02:44 +0200

    Add function

--- x/gamm/keeper/export_test.go
+++ x/gamm/keeper/export_test.go
@@ -3,11 +3,7 @@ package keeper
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
-<<<<<<< HEAD
 	"github.com/osmosis-labs/osmosis/v10/x/gamm/types"
-=======
-	"github.com/osmosis-labs/osmosis/v11/x/gamm/types"
->>>>>>> ccc5afe (Add function)
 )
 
 // SetParams sets the total set of params.
