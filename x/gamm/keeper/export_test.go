ccc5afe6c4c513ea1bff97840ae4c37d1c9fd879
Niccolo Raspa <nraspa@live.it>
2022-07-29 05:56:38 +0200

    Add function

--- x/gamm/keeper/export_test.go
+++ x/gamm/keeper/export_test.go
@@ -2,10 +2,16 @@ package keeper
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+
 	"github.com/osmosis-labs/osmosis/v11/x/gamm/types"
 )
 
 // SetParams sets the total set of params.
 func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
+	k.setParams(ctx, params)
+}
+
+// SetParams sets the total set of params.
+func (k Keeper) SetParams2(ctx sdk.Context, params types.Params) {
 	k.setParams(ctx, params)
 }
