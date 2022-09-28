92e394a8bbaa0b61ab4d09fa1fb53acb36da28df
Niccolo Raspa <nraspa@live.it>
2022-09-28 16:24:59 +0200

    feat: new-func

--- x/gamm/keeper/export_test.go
+++ x/gamm/keeper/export_test.go
@@ -20,3 +20,8 @@ func (k Keeper) SetParams2(ctx sdk.Context, params types.Params) {
 func (k Keeper) SetParams2(ctx sdk.Context, params types.Params) {
 	k.setParams(ctx, params)
 }
+
+// SetParams sets the total set of params.
+func (k Keeper) SetParams2(ctx sdk.Context, params types.Params) {
+	k.setParams(ctx, params)
+}
